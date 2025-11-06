17446a9dfe1c1c8635bfca5e09a218d36b199087
Norman Huth <norman@huth.it>
2025-11-06 23:16:12 +0100

    chore: include app environment in Redis and cache prefixes, update .env example

--- config/database.php
+++ config/database.php
@@ -148,7 +148,8 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) .
+                Str::slug((string) env('APP_ENV', 'production')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
