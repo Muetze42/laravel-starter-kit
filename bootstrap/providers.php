885f18faae04691bc172219351e79b3c7c28c047
Norman Huth <norman@huth.it>
2025-12-03 17:26:43 +0100

    chore: add MigrationServiceProvider with custom MigrationCreator and introduce MacroRegistryService for macro management

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FrontendServiceProvider::class,
     App\Providers\HttpClientProvider::class,
+    App\Providers\MigrationServiceProvider::class,
 ];
