3e0325cd36a06da260125da1bd46ae20abf4c12d
Norman Huth <norman@huth.it>
2025-12-06 09:45:21 +0100

    chore: add DatabaseServiceProvider with custom PostgresGrammar for improved date format handling in PostgreSQL

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\DatabaseServiceProvider::class,
     App\Providers\FrontendServiceProvider::class,
     App\Providers\HttpClientProvider::class,
     App\Providers\MigrationServiceProvider::class,
