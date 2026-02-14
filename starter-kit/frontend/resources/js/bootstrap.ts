8d4be6372be2f9781c0178ea729680909756c4b3
Norman Huth <norman@huth.it>
2026-02-14 15:30:05 +0100

    chore: simplefy resources

--- starter-kit/frontend/resources/js/bootstrap.ts
+++
@@ -1,4 +0,0 @@
-import Axios from 'axios'
-window.axios = Axios.create()
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
--- starter-kit/frontend/resources/js/types/app.d.ts
+++
@@ -1,6 +0,0 @@
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-  // config: ConfigInterface
-  // currentUser: CurrentUserInterface | null
-  // httpStatus: HttpStatusInterface
-  // translations: { [key: string]: string }
-}
