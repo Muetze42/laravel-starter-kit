2b0260ae441924dd41ed1235bf78078a6e579594
Norman Huth <norman@huth.it>
2026-03-07 22:15:21 +0100

    chore: remove unused starter-kit configuration and assets

--- starter-kit/frontend/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use NormanHuth\Library\Http\Controllers\Api\SentryTunnelController;
-
-Route::get('/', fn () => response()->json(['message' => 'It works!']));
-Route::post('sentry-tunnel', SentryTunnelController::class);
