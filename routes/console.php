aff64e792f63261f9c30447d692f21ac132c2300
Norman Huth <norman@huth.it>
2026-03-09 20:27:39 +0100

    chore: add commented schedule commands in console routes for future task automation

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function (): void {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// \Illuminate\Support\Facades\Schedule::command('model:prune')->daily();
+// \Illuminate\Support\Facades\Schedule::command('horizon:snapshot')->everyFiveMinutes();
+// \Illuminate\Support\Facades\Schedule::command('auth:clear-resets')->everyFifteenMinutes();
+// \Illuminate\Support\Facades\Schedule::command('telescope:prune')->daily();
+// \Illuminate\Support\Facades\Schedule::command('sanctum:prune-expired --hours=24')->daily();
+// \Illuminate\Support\Facades\Schedule::command('passport:purge')->hourly();
+// \Illuminate\Support\Facades\Schedule::command('queue:prune-batches')->daily();
