892586c85c58d9c2aa15b8d1f34eb69bf4fd8fd6
Norman Huth <norman@huth.it>
2026-05-11 20:53:50 +0200

    chore: Update Tailwind configuration and Vite setup with additional fonts

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,23 @@
 import {defineConfig} from 'vite'
 import laravel from 'laravel-vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
+import { bunny } from 'laravel-vite-plugin/fonts'
 
 export default defineConfig({
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
+      fonts: [
+        bunny('Inter', {
+          weights: [400, 500, 600],
+        }),
+        bunny('JetBrains Mono', {
+          weights: [400, 500, 600],
+        }),
+      ],
       refresh: true
     }),
+    // inertia(),
     tailwindcss(),
     // sentryVitePlugin({
     //   org: 'norman-huth',
@@ -19,10 +29,21 @@ export default defineConfig({
     //   },
     //   authToken: env.VITE_SENTRY_AUTH_TOKEN.trim()
     // })
+    // vue({
+    //   template: {
+    //     transformAssetUrls: {
+    //       base: null,
+    //       includeAbsolute: false,
+    //     },
+    //   },
+    // }),
+    // wayfinder({
+    //   formVariants: true,
+    // }),
   ],
-  server: {
-    watch: {
-      ignored: ['**/storage/framework/views/**']
-    }
-  }
+  // server: {
+  //   watch: {
+  //     ignored: ['**/storage/framework/views/**']
+  //   }
+  // }
 })
