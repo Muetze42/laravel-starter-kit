7874a826c61eb504a6a00561597c46c868386e2e
Norman Huth <norman@huth.it>
2026-09-05 14:09:51 +0200

    feat: add overridden PolicyMakeCommand to enforce suffix validation

---
+++ app/Console/Commands/Development/Overrides/PolicyMakeCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands\Development\Overrides;
+
+use App\Console\Commands\Concerns\ArgumentNameSuffixTrait;
+use Illuminate\Foundation\Console\PolicyMakeCommand as Command;
+use Override;
+
+class PolicyMakeCommand extends Command
+{
+    use ArgumentNameSuffixTrait;
+
+    /**
+     * Retrieves the suffix for the argument name.
+     */
+    public function getSuffix(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    #[Override]
+    public function handle(): ?bool
+    {
+        $this->ensureNameHasSuffix();
+
+        return parent::handle();
+    }
+}
