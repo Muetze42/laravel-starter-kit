f855714882616586a87f1151d43364be57528c8c
Norman Huth <norman@huth.it>
2026-09-05 14:23:38 +0200

    feat: add overridden FactoryMakeCommand to enforce suffix validation

---
+++ app/Console/Commands/Development/Overrides/FactoryMakeCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands\Development\Overrides;
+
+use App\Console\Commands\Concerns\ArgumentNameSuffixTrait;
+use Illuminate\Database\Console\Factories\FactoryMakeCommand as Command;
+use Override;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'make:factory')]
+class FactoryMakeCommand extends Command
+{
+    use ArgumentNameSuffixTrait;
+
+    /**
+     * Retrieves the suffix for the argument name.
+     */
+    public function getSuffix(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    #[Override]
+    public function handle(): ?bool
+    {
+        $this->ensureNameHasSuffix();
+
+        return parent::handle();
+    }
+}
