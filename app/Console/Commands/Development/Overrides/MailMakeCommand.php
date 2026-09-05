f5196060d96f1776db824d9f60959184ea15e0dc
Norman Huth <norman@huth.it>
2026-09-05 14:22:10 +0200

    feat: add overridden MailMakeCommand to enforce suffix validation

---
+++ app/Console/Commands/Development/Overrides/MailMakeCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands\Development\Overrides;
+
+use App\Console\Commands\Concerns\ArgumentNameSuffixTrait;
+use Illuminate\Foundation\Console\MailMakeCommand as Command;
+use Override;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'make:mail')]
+class MailMakeCommand extends Command
+{
+    use ArgumentNameSuffixTrait;
+
+    /**
+     * Retrieves the suffix for the argument name.
+     */
+    public function getSuffix(): string
+    {
+        return 'Mail';
+        // return $this->type;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    #[Override]
+    public function handle(): ?bool
+    {
+        $this->ensureNameHasSuffix();
+
+        return parent::handle();
+    }
+}
