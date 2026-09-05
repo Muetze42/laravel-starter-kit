c9d2abf0b9eae17f2b924df232037e34ef9ec79e
Norman Huth <norman@huth.it>
2026-09-05 14:11:37 +0200

    feat: add overridden SeederMakeCommand to enforce suffix validation

---
+++ app/Console/Commands/Development/Overrides/SeederMakeCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands\Development\Overrides;
+
+use App\Console\Commands\Concerns\ArgumentNameSuffixTrait;
+use Illuminate\Database\Console\Seeds\SeederMakeCommand as Command;
+use Override;
+
+class SeederMakeCommand extends Command
+{
+    use ArgumentNameSuffixTrait;
+
+    /**
+     * Retrieves the suffix for the argument name.
+     */
+    public function getSuffix(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    #[Override]
+    public function handle(): void
+    {
+        $this->ensureNameHasSuffix();
+
+        parent::handle();
+    }
+}
