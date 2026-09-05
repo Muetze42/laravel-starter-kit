e6604f5a348e9661b3f3a54f8a650cd52c20300f
Norman Huth <norman@huth.it>
2026-09-05 05:45:25 +0200

    chore: enforce global HTTP client options and prevent stray requests during unit tests in HttpClientServiceProvider

--- app/Providers/HttpClientServiceProvider.php
+++ app/Providers/HttpClientServiceProvider.php
@@ -15,11 +15,32 @@ class HttpClientServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configureGlobalOptions();
+        $this->preventStrayRequests();
+    }
+
+    /**
+     * Configure global HTTP client options.
+     */
+    protected function configureGlobalOptions(): void
+    {
         Http::globalOptions([
             'headers' => [
-                'User-Agent' => Config::string('app.name') . ' ' . Config::string('app.env'),
+                'User-Agent' => Config::string('app.name'),
                 'X-Environment' => Config::string('app.env'),
             ],
         ]);
+    }
+
+    /**
+     * Prevents stray requests during unit tests.
+     */
+    protected function preventStrayRequests(): void
+    {
+        if (! $this->app->runningUnitTests()) {
+            return;
+        }
+
+        Http::preventStrayRequests();
     }
 }
